deb9eded03826916cb8d3cc2a8bf1d8c40ddbe90
azad-mammedov <azadmammedov6@gmail.com>
2025-12-07 03:37:09 +0400

    new docker adjustments

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,10 +1,19 @@
 #!/usr/bin/env bash
 set -e
 
+echo "Waiting for PostgreSQL at ${DB_HOST}:${DB_PORT}..."
+for i in {1..30}; do
+  if pg_isready -h "${DB_HOST}" -p "${DB_PORT}" -U "${DB_USER}" -d "${DB_NAME}" > /dev/null 2>&1; then
+    echo "Postgres is ready"
+    break
+  fi
+  echo -n "."
+  sleep 1
+done
+
+
 python -c "import os; print('Using settings:', os.getenv('DJANGO_SETTINGS_MODULE','backend.settings'))"
 
-# DB & static
-python manage.py makemigrations --noinput
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
 
